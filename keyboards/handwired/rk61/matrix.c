4d468df23daa307171001e567de70f6ca424669f
Cameron Strandlund <cstrandlund@gmail.com>
2020-11-22 08:17:10 -0700

    Added LED control to keyboard/matrix.c. CAPS lock and backlight operate independantly now.

--- keyboards/handwired/rk61/matrix.c
+++ keyboards/handwired/rk61/matrix.c
@@ -26,6 +26,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "debounce.h"
 #include "quantum.h"
 
+#define RK61_BACKLIGHT F4
+
 static const pin_t row_pins[MATRIX_ROWS] = MATRIX_ROW_PINS;
 static const pin_t col_pins[MATRIX_COLS] = MATRIX_COL_PINS;
 
@@ -53,6 +55,8 @@ static void init_pins(void) {
     for (uint8_t x = 0; x < MATRIX_COLS; x++) {
         setPinInputHigh(col_pins[x]);
     }
+    setPinOutput(RK61_BACKLIGHT);
+    writePinLow(RK61_BACKLIGHT);
 }
 
 static void select_col(uint8_t col) {
@@ -62,10 +66,43 @@ static void select_col(uint8_t col) {
 
 static void unselect_col(uint8_t col) { setPinInputHigh(col_pins[col]); }
 
+void led_update(int col_index, uint8_t current_row)
+{
+    int min_col;
+    bool caps_lock  = IS_HOST_LED_ON(USB_LED_CAPS_LOCK);
+    int is_bl = is_backlight_enabled();
+    if(current_row==2)
+    {
+        if(is_bl)
+        { // backlight is on so make sure all columns are lit (except CAPS lock)
+            writePinHigh(RK61_BACKLIGHT); // output for LED row 2
+            if(caps_lock)
+                min_col = 0;
+            else
+                min_col = 1;
+            // set LED column low (on) for one loop
+            if(col_index>=min_col) select_col(col_index); // set column to output low to turn on LED if ROWLED line is low
+        }
+        else // backlight off so only turn on CAPS lock if needed
+        {
+            if(caps_lock && col_index==0)
+            {
+                writePinHigh(RK61_BACKLIGHT); // output for LED row 2
+                select_col(col_index); // set column to output low to turn on CAPS LED if ROWLED line is low
+            }
+        }
+    }
+    else writePinLow(RK61_BACKLIGHT); // Turn off LED row
+
+    unselect_col((col_index>0?col_index-1:MATRIX_COLS-1)); // set previous column back to input pullup.
+
+    // After the column loop, something else happens that causes the last column to remain lit for longer than the rest
+    //   this causes it to look brighter than the other keys.
+}
+
 static bool read_cols_on_row(matrix_row_t current_matrix[], uint8_t current_row) {
     // Start with a clear matrix row
     matrix_row_t current_row_value = 0;
-
     // Select row and wait for row selecton to stabilize
     select_row(current_row);
     matrix_io_delay();
@@ -74,14 +111,9 @@ static bool read_cols_on_row(matrix_row_t current_matrix[], uint8_t current_row)
     for (uint8_t col_index = 0; col_index < MATRIX_COLS; col_index++) {
         // Select the col pin to read (active low)
         uint8_t pin_state = readPin(col_pins[col_index]);
+        led_update(col_index,current_row);
         // Populate the matrix row with the state of the col pin
         current_row_value |= pin_state ? 0 : (MATRIX_ROW_SHIFTER << col_index);
-
-        // set LED column 0 low (on) for one loop
-        select_col(col_index); // set column to output low to turn on LED if ROWLED line is low
-        unselect_col((col_index>0?col_index-1:MATRIX_COLS-1)); // set previous column back to input pullup.
-        // After the column loop, something else happens that causes the last column to remain lit for longer than the rest
-        //   this causes it to look brighter than the other keys.
     }
 
     // Unselect row
